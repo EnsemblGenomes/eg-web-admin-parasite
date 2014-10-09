8e4696da37f6d9869f3bdcf61532f9523243c0c2
Bruce Bolt <bbolt@ebi.ac.uk>
2014-10-09 16:51:13 +0100

    Modified primary key on StaticSpecies.pm

--- modules/EnsEMBL/ORM/Rose/Object/StaticSpecies.pm
+++ modules/EnsEMBL/ORM/Rose/Object/StaticSpecies.pm
@@ -18,11 +18,11 @@ __PACKAGE__->meta->setup(
 
   columns       => [
     species_name => {type => 'varchar', 'length' => 50, primary_key => 1, not_null => 1},
-    display_name => {type => 'varchar', 'length' => 50, primary_key => 1, not_null => 1},
+    display_name => {type => 'varchar', 'length' => 50, not_null => 1},
     description  => {type => 'text'},
   ],
 
-  title_column  => 'display_name',
+  title_column  => 'species_name',
 
   unique_key    => ['species_name'],
 
